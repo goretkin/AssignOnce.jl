85923264ad4e58b0d93a31b875e7cfec780d9124
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-12-08 20:41:23 -0500

    Solution

--- src/AssignOnce.jl
+++ src/AssignOnce.jl
@@ -1,3 +1,48 @@
 module AssignOnce
 
+export @assignonce
+
+import MacroTools
+
+"""
+match e.g. `x = rhs` but not e.g. `(x, y) = rhs`
+"""
+function match_simple_assignment(ex)
+    MacroTools.@capture(ex,
+        lhs_Symbol = rhs_
+    )
+    return (lhs=lhs, rhs=rhs)
+end
+
+"""
+If you want to execute the same script multiple times as you develop it, but have a few expensive statements that need to be executed just once (e.g. load a dataset) you can do
+
+`@assignonce x = load_data()`
+
+TODO: give error if not called from a global (i.e. module) scope
+"""
+macro assignonce(ex)
+  global lhs
+  (lhs, rhs) = match_simple_assignment(ex)
+
+  assignment = :($lhs = $(rhs))
+  @assert assignment == ex
+  # quote to avoid macro hygiene since these expressions are to be evaluated in the macro invocation scope
+  lhs_quote = QuoteNode(lhs)
+  ex_quote = QuoteNode(ex)
+  # expand string interpolations at parse time: https://github.com/JuliaLang/julia/issues/1557#issuecomment-10503787
+  exec_msg = "executing $ex"
+  skip_msg = "skipping $ex"
+  quote
+    if !isdefined(@__MODULE__, $lhs_quote)
+      println($exec_msg)
+      (@__MODULE__).eval($ex_quote)
+      println("\tcomplete")
+    else
+      println($skip_msg)
+    end
+  end
+end
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,57 @@
 using AssignOnce
 using Test
+
+
+struct InvocationCounter{F}
+    f::F
+    counter::Ref{Int}
+end
+
+InvocationCounter(f) = InvocationCounter(f, Ref(0))
+
+function (c::InvocationCounter)(args...; kwargs...)
+    c.counter[] += 1
+    c.f(args...; kwargs...)
+end
+
+
+@testset "check test purity 1" begin
+    y = 3
+end
+
+@testset "check test purity 2" begin
+    @test !isdefined(@__MODULE__, :y)
+end
+
+@testset "InvocationCounter" begin
+    f = InvocationCounter(()->3)
+    @test f.counter[] == 0
+    @test f() == 3
+    @test f.counter[] == 1
+    @test f() == 3
+    @test f.counter[] == 2
+end
+
+
+# TODO maybe generate a new module, so there is a new global scope, for each test set
+@testset "does assignment" begin
+    @assert !isdefined(@__MODULE__, :x)
+    @assignonce x = 3
+    @test isdefined(@__MODULE__, :x)
+    @test x == 3
+end
+
+# `x` is used up.
+# TODO, same thing. would be nice to have a new module scope so we can define `ff` in there
+
+ff = InvocationCounter(()->3)
+@testset "does assignment only once" begin
+    @assert !isdefined(@__MODULE__, :y)
+    @assert ff.counter[] == 0
+    @assignonce y = ff()
+    @test ff.counter[] == 1
+    @assignonce y = ff()
+    @test ff.counter[] == 1
+    @test isdefined(@__MODULE__, :y)
+    @test y == 3
+end
